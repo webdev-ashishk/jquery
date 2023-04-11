8969d3ccdc0b6e43b3a4613b31734b9d29c9e930
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-11 10:42:25 +0530

    added multi css property

--- jquery-html/script.js
+++ jquery-html/script.js
@@ -3,9 +3,30 @@ console.log("welcome@");
 $(document).ready(function () {
   //-------------jquery CSS()------------------>
 
+  //<------ return a css property ------->
   $("#pbtn").click(() => {
     const c = $("#para").css("color");
     alert("done" + c);
     // console.log(color);
+  });
+  //<------ set css property ------->
+
+  $("#pbtn").click(() => {
+    $("#para").css("color", "blue");
+  });
+  //<------ setMultiple css property ------->
+
+  $("#pbtn").click(() => {
+    $("#para").css({
+      border: "2px solid green",
+      "background-color": "black",
+      "border-radius": "5px",
+      "font-size": "5rem",
+    });
+  });
+
+  // <----------css classes------------------>
+  $("#aclassbtn").click(() => {
+    $("p").addClass("blue");
   });
 });
